c45b78bdcd18cf8d75459db488fd27dabd9a118e
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-11 18:31:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog 
-  def initialize(breed)
-    @breed = breed
-  end
+  # def initialize(breed)
+  #   @breed = breed
+  # end
   
-  def breed
-    @breed
-  end
+  # def breed
+  #   @breed
+  # end
 
   def breed=breed
     @breed = breed 
